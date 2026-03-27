81d90e2fcb20753a5aa1670970918659d6bbeb47
Manuel Alcantara Juarez <manuelalcantara@MacBook-Pro-de-Manuel.local>
2026-03-27 08:42:59 -0600

    manuel: capitulo 1 y 2

--- tex/codes/codeforces/231A.cpp
+++ tex/codes/codeforces/231A.cpp
@@ -1,20 +1,20 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;cin>>n;
+int main() {
+    int n;
+    cin >> n;
     int implementados = 0;
-    for(int i = 0; i < n; i++){
-        int contador = 0;
-        for(int j = 0; j < 3; j++){
-            int temp; cin>>temp;
-            if(temp == 1){
-                contador++;
-            }
-        }
-        if(contador >= 2){
+
+    for (int i = 0; i < n; i++) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        // Si al menos dos estan seguros, se implementa el problema.
+        if (a + b + c >= 2) {
             implementados++;
         }
     }
-    cout<<implementados<<"\n";
+
+    cout << implementados << "\n";
 }
